24a3cddceb2c7efc915ec153d999da014d908554
竹内 将悟 <takeuchishogo@team-lab.com>
2019-10-29 01:24:22 +0900

    first commit

---
+++ docs/precache-manifest.b13cbeaa920fa6267731f5293d22f3ac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74e3a99cc3cd4ced2431",
+    "url": "/css/app.1955fed0.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "6341d1631f6c77d8314dbed7d3eb803b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c7144d9543820cda5a1",
+    "url": "/js/about.643d4b7b.js"
+  },
+  {
+    "revision": "74e3a99cc3cd4ced2431",
+    "url": "/js/app.f9722b54.js"
+  },
+  {
+    "revision": "bcc700f3710c95e17934",
+    "url": "/js/chunk-vendors.437d1d12.js"
+  },
+  {
+    "revision": "9385a278273e8420051eefe0bee03179",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hello">
+    <p>
+      Hello!!
+      I'm {{name}}
+    </p>
+    <p><a href="https://github.com/takeucheese" target="_blank" rel="noopener">GitHub</a></p>
+    <p><a href="https://twitter.com/hello_ts74" target="_blank" rel="noopener">Twitter</a></p>
+    <p><a href="https://www.wantedly.com/users/113967751" target="_blank" rel="noopener">Wantedly</a></p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private name!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
